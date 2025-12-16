c3b27e10094cef9a90a7c6e5ba8570a34fafe69e
GiovanniWitulski <witulski.schule@gmail.com>
2025-12-16 17:15:51 +0100

    feat: add user registration endpoint and CustomUser model
    - Added CustomUser model extending AbstractUser.
    - Registered CustomUser in Django Admin.
    - Implemented RegistrationSerializer with:
      - Password confirmation validation.
      - Unique email check.
    - Added RegisterView API endpoint handling POST requests.
    - Configured URL routing for registration.

---
+++ auth_app/api/serializers.py
@@ -0,0 +1,33 @@
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+class RegistrationSerializer(serializers.ModelSerializer):
+    confirmed_password = serializers.CharField(style={'input_type': 'password'}, write_only=True)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password', 'confirmed_password']
+        extra_kwargs = {
+            'password': {'write_only': True}
+        }
+
+    def validate(self, data):
+        if data['password'] != data['confirmed_password']:
+            raise serializers.ValidationError({"password": "The passwords do not match."})
+        
+        if User.objects.filter(email=data['email']).exists():
+            raise serializers.ValidationError({"email": "this email is already in use."})
+            
+        return data
+
+    def create(self, validated_data):
+        validated_data.pop('confirmed_password')
+        
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            password=validated_data['password']
+        )
+        return user
---
+++ auth_app/api/views.py
@@ -0,0 +1,19 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import RegistrationSerializer
+
+class RegisterView(APIView):
+    permission_classes = [] 
+
+    def post(self, request):
+        serializer = RegistrationSerializer(data=request.data)
+        
+        if serializer.is_valid():
+            serializer.save()
+            return Response(
+                {"detail": "User created successfully!"}, 
+                status=status.HTTP_201_CREATED
+            )
+            
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
