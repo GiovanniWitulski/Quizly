e06438592e3881260ab645c0a85925aa25a5c1c4
GiovanniWitulski <witulski.schule@gmail.com>
2025-12-16 17:47:43 +0100

    build: register SimpleJWT and blacklist apps in settings
    - Added 'rest_framework_simplejwt' to INSTALLED_APPS for JWT authentication.
    - Added 'rest_framework_simplejwt.token_blacklist'

--- core/settings.py
+++ core/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'corsheaders',
+    'rest_framework_simplejwt',
+    'rest_framework_simplejwt.token_blacklist',
     'auth_app',
     'quizzes_app',
 ]
