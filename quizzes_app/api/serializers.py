26082a6386d7059775e5fdfa7dee7639f3d7d340
GiovanniWitulski <witulski.schule@gmail.com>
2025-12-23 05:02:29 +0100

    feat: add serializers for listing quizzes and questions
    - Created 'QuizListSerializer' with nested question support.
    - Created 'QuestionListSerializer' for read-only display.

--- quizzes_app/api/serializers.py
+++ quizzes_app/api/serializers.py
@@ -6,8 +6,20 @@ class QuestionSerializer(serializers.ModelSerializer):
         model = Question
         fields = ['id', 'question_title', 'question_options', 'answer', 'created_at', 'updated_at']
 
+class QuestionListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Question
+        fields = ['id', 'question_title', 'question_options', 'answer']
+
 class QuizDetailSerializer(serializers.ModelSerializer):
     questions = QuestionSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Quiz
+        fields = ['id', 'title', 'description', 'created_at', 'updated_at', 'video_url', 'questions']
+
+class QuizListSerializer(serializers.ModelSerializer):
+    questions = QuestionListSerializer(many=True, read_only=True)
 
     class Meta:
         model = Quiz
